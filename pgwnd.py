f0d4f3e94f42c46c2e3d814375db3356bb5204a7
Paco Barter <paco.barter@gmail.com>
2011-04-17 22:42:41 +0200

    ++ se mueve la actualizacion del frame y la gestion de eventos a funciones externas (patron Delegate)

--- pgwnd.py
+++ pgwnd.py
@@ -3,44 +3,34 @@ import random
 
 class PGWindow:
 
-    def __init__(self,w,h):
+    def __init__(self,w,h,frame_updater,event_manager):
         pygame.init() 
 
         self.screen = pygame.display.set_mode([w,h])
-        self.screen.fill([255,255,255])
 
         self.mainloop = True
-        self.x = 25
-        self.y = 0
-        self.color = (32,32,32)
-        self.fontsize = 1
-        self.delta = 0
+        
         self.fps = 30
         self.clock = pygame.time.Clock()
 
+        self.frame_updater=frame_updater
+        self.event_manager=event_manager
+        
 
     def loop(self):
+        # bucle ppal
         while self.mainloop:
+            # actualizamos el tiempo
             tick_time = self.clock.tick(self.fps) # milliseconds since last frame
 
-            pygame.display.set_caption("press Esc to quit. FPS: %.2f" % (self.clock.get_fps()))
+            # funcion de actualizacion
+            self.frame_updater(self.screen, self.clock)
 
-            self.fontsize = random.randint(35, 150)
-            myFont = pygame.font.SysFont("None", self.fontsize)
-            self.color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
-
-            self.screen.fill((255,255,255))
-
-            self.screen.blit(myFont.render("I love the pygame cookbook", 0, (self.color)), (self.x,self.y))
-
+            # funcion de gestion de eventos
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.mainloop = False
+                self.mainloop=self.event_manager(event)
 
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        self.mainloop = False
-
+            # update del display
             pygame.display.update()
 
         pygame.quit()        
--- test2.py
+++ test2.py
@@ -1,9 +1,46 @@
+import pygame
+import random
 import pgwnd
 
-wnd=pgwnd.PGWindow(640,480)
+x=25
+y=0
+color=(32,32,32)
+back_color=(255,255,255)
 
-wnd.loop()
+#
+#   Actualizacion del frame
+#
+def update_frame(screen,clock):
+    pygame.display.set_caption("press Esc to quit. FPS: %.2f" % (clock.get_fps()))
+
+    fontsize = random.randint(35, 150)
+    myFont = pygame.font.SysFont("None", fontsize)
+    color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+    screen.fill(back_color)
+
+    screen.blit(myFont.render("I love the pygame cookbook", 0, (color)), (x,y))
+
+#
+#   Gestion de eventos
+#
+def event_manager(event):
+    if event.type == pygame.QUIT:
+        return False
+
+    elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+            return False
+            
+    else:
+        return True
 
 
+#--------------------------------------------------------------
+#       MAIN
+#--------------------------------------------------------------
+if __name__=='__main__':
+    wnd=pgwnd.PGWindow(640,480,update_frame,event_manager)
+    wnd.loop()
 
 
